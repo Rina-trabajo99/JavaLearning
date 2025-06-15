8a2246f624e3bf7837900d136432c6d25e6d6378
Rina <Rina@192.168.2.112>
2025-06-15 11:28:56 +0900

    test

---
+++ HelloWorld_RinaKobayashi/src/Study/Basic.java
@@ -0,0 +1,24 @@
+package Study;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//氏名
+		String name = "小林 里奈";
+		//年齢
+		int age = 26;
+		//趣味
+		String hobby = "映画鑑賞";
+		
+		/*
+		 * 自己紹介文を出力させる処理
+		 */
+			
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("年齢は" + age + "です");
+		System.out.println("趣味は" + hobby + "です");
+	}
+
+}
