67a08b828452bc9667ba370f96715f8abadaa81e
Rina <Rina@192.168.2.112>
2025-06-15 23:15:58 +0900

    Java1.4

--- HelloWorld_RinaKobayashi/src/curriculum_A/Qes1_13.java
+++ HelloWorld_RinaKobayashi/src/curriculum_A/Qes1_13.java
@@ -4,6 +4,36 @@ public class Qes1_13 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		/*
+		問題１
+		下記9個をローカル変数として宣言のみしてください
+		・バイト型・短整数型・整数型・長整数型
+		・単精度浮動小数点数型・倍精度浮動小数点数型
+		・文字型・文字列型
+		・ブーリアン型
+		 */
+		
+		//バイト型
+		byte Byte;
+		//短整数型
+		short Short;
+		//整数型
+		int Int;
+		//長整数型
+		long Long;
+		//単精度浮動小数点型
+		float Float;
+		//倍精度浮動小数点型
+		double Double;
+		//文字型
+		char Char;
+		//文字列型
+		String String;
+		//ブーリアン型(真偽値)
+		boolean Boolean;
+		
+			
 
 	}
 
